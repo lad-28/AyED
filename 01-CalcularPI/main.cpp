a08141b66fbf7d8d5514275f5a3acff520ae3a6c
lad-28 <133971817+lad-28@users.noreply.github.com>
2023-06-25 22:37:56 -0300

    Corrección del TP.cpp
    Cambios:
    - Se modificó el algoritmo para que calcule 3.141592 con precisión.
    - Se cambió "system(pause)" por "cin.get()" para más alcance en los SO.

--- 01-CalcularPI/main.cpp
+++ 01-CalcularPI/main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include <cmath>
 #include <stdlib.h>
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
-    double i=0,pi=0,serie=0;
+    float i=0, serie=0, pi=0, aux=0;
+       do
+        {
+           serie += pow((-1),i)/((2*i)+1);
+           pi = 4*serie;
+           aux = abs(pi-3.141592);
+           i+=1;
+        }
+        while (aux > 0.0000001);
 
-    for (i;i<= 1599999; i++)
-    {
-        serie+= pow((-1),i)/((2*i)+1);
-
-        pi= serie*4;
-    }
-
-cout.precision(8);
+cout.precision(7);
 
 cout<<"El valor de pi es: "<<pi<<endl;
 
 cout<<"El numero de iteraciones para llegar al valor es de: "<<i-1<<endl;
 
-    system("PAUSE");
+    cin.get();
     return 0;
 }
